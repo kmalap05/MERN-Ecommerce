68f6a1e759c371c24bb645727649dc4fb0abb0ad
kmalap05 <83391228+kmalap05@users.noreply.github.com>
2023-08-02 19:09:11 +0530

    Database Error Resolved!

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,7 +4,6 @@ const connectDatabase = () => {
   mongoose
     .connect(process.env.DB_URI, {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
     })
     .then((data) => {
       console.log(`Mongodb connected with server: ${data.connection.host}`);
